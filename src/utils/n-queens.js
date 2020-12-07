eae074b8013ffcb5249185f75067f1252770dcc5
Lucas <siqueiralpontes@gmail.com>
2020-12-07 15:38:18 -0300

    mudanças no código

--- src/utils/n-queens.js
+++ src/utils/n-queens.js
@@ -3,8 +3,11 @@
 export default function nRainhas(n) {
     var rainhas = [];
     var colunas = [];
+    var tabuleiro = [];
+
+    tabuleiro[0] = [];
     for (var j = 0; j < n; j++) {
-        colunas[j] = {
+        tabuleiro[0][j] = {
             estado: 'v',
             diagD: -1,
             diagE: -1
@@ -14,9 +17,11 @@ export default function nRainhas(n) {
     var i = 0;
     var colunaVazia = -1;
     var coluna = -1;
+    let colRainhaRetirada;
+
     while (i < n) {
-        for (var j = 0; j < colunas.length; j++) {
-            if (colunas[j].estado == 'v') {
+        for (var j = 0; j < tabuleiro[i].length; j++) {
+            if (tabuleiro[i][j].estado == 'v') {
                 colunaVazia = j;
                 break;
             }
@@ -24,73 +29,82 @@ export default function nRainhas(n) {
         //posicionando a rainha
         if (colunaVazia > -1) {
             coluna = colunaVazia; //verificar se isso so aponta pra colunaVazia ou se copia
-            rainhas.push({ lin: i, col: coluna });
-            colunas[coluna].estado = 'o';
-            colunas[coluna].diagD = i;
-            colunas[coluna].diagE = i;
             //andando as diagonais da proxima linha
-            auxColunas = JSON.parse(JSON.stringify(colunas));
-            for (var j = 0; j < colunas.length; j++) { //acho que tem que desmarcar aqui
-                if (colunas[j].estado == 'd')
-                    auxColunas[j].estado = 'v';
-                if (j > 0) {
-                    if (colunas[j].diagE >= 0) {
-                        auxColunas[j - 1].diagE = colunas[j].diagE;
-                        auxColunas[j].diagE = -1;
-                        if (colunas[j - 1].estado == 'v')
-                            auxColunas[j - 1].estado = 'd'; //talvez tenha que fazer só quando o estado é vazio
+            rainhas.push({ lin: i, col: coluna });
+            tabuleiro[i][coluna].estado = 'o';
+            tabuleiro[i][coluna].diagD = i;
+            tabuleiro[i][coluna].diagE = i;
+
+            auxColunas = JSON.parse(JSON.stringify(tabuleiro[i]));
+            for(let k=0; k<auxColunas.length; k++) {
+                if (auxColunas[k].estado == 'm') auxColunas[k].estado = 'v'
+            }
+            
+            for (var j = 0; j < tabuleiro[i].length; j++) { //acho que tem que desmarcar aqui
+                if (j < tabuleiro[i].length - 1) {
+                    auxColunas[j + 1].diagD = tabuleiro[i][j].diagD;
+                    if (j == 0) {
+                        auxColunas[j].diagD = -1;
+                        if (tabuleiro[i][j].estado == 'v') auxColunas[j].estado = 'v';
+                    }
+                    if (tabuleiro[i][j+1].estado == 'v') {
+                        if (tabuleiro[i][j].diagD > -1 ) {
+                            auxColunas[j+1].estado = 'd';
+                        } else {
+                            auxColunas[j+1].estado = 'v';
+                        }
+                    }
+                    if (tabuleiro[i][j].estado == 'd') { //acho que esse if esta sendo feito na linha 65
+                        auxColunas[j].estado = 'v'
                     }
                 }
-                if (j < colunas.length - 1) {
-                    if (colunas[j].diagD >= 0) {
-                        auxColunas[j + 1].diagD = colunas[j].diagD;
-                        auxColunas[j].diagD = -1;
-                        if (colunas[j + 1].estado == 'v')
-                            auxColunas[j + 1].estado = 'd'; //talvez tenha que fazer só quando o estado é vazio
+                if (j > 0) {
+                    auxColunas[j - 1].diagE = tabuleiro[i][j].diagE;
+                    if (j == tabuleiro[i].length-1) {
+                        auxColunas[j].diagE = -1;
+                        if (auxColunas[j].estado == 'v') auxColunas[j].estado = 'v';
                     }
+                    if (auxColunas[j-1].estado == 'v') {
+                        if (tabuleiro[i][j].diagE > -1 ) {
+                            auxColunas[j-1].estado = 'd';
+                        } /* else {
+                            if (auxColunas[j-1].estado == 'v') auxColunas[j-1].estado = 'v';
+                        } */
+                    }
+                    //if (tabuleiro[i][j].estado == 'd' && auxColunas[j].estado == '')
                 }
             }
             //mandar a pagina mostrar a linha retornando o vetor rainhas e talvez colunas também
             //desenha(rainhas, i, colunas, "insere");
-            colunas = JSON.parse(JSON.stringify(auxColunas));
             i++;
+            tabuleiro[i] = JSON.parse(JSON.stringify(auxColunas));
             colunaVazia = -1;
         }
         else {
             i--;
-            if (i == 0)
+            if (i < 0)
                 i = n + 1;
             else {
-                //voltando com as diagonais da linha anterior
-                auxColunas = JSON.parse(JSON.stringify(colunas));
-                for (var j = 0; j < colunas.length; j++) {
-                    // se colunas[j].estado == 'd' então auxColunas[j].estado = 'v'
-                    if (colunas[j].estado == 'd')
-                        auxColunas[j].estado = 'v';
-                    if (j > 0) {
-                        if (colunas[j].diagD >= 0) {
-                            auxColunas[j - 1].diagD = colunas[j].diagD;
-                            auxColunas[j].diagD = -1;
-                            if (colunas[j - 1].estado == 'v')
-                                auxColunas[j - 1].estado = 'd'; //talvez tenha que fazer só quando o estado é vazio
+                if (i > 0) {
+                    tabuleiro.pop();
+                    for(let j=0; j<tabuleiro[i].length; j++) {
+                        if(tabuleiro[i][j].diagE > -1 && tabuleiro[i][j].diagE == tabuleiro[i][j].diagE) {
+                            colRainhaRetirada = j;
                         }
                     }
-                    if (j < colunas.length - 1) {
-                        if (colunas[j].diagE >= 0) {
-                            auxColunas[j + 1].diagE = colunas[j].diagE;
-                            auxColunas[j + 1].diagE = -1;
-                            if (colunas[j + 1].estado == 'v')
-                                auxColunas[j + 1].estado = 'd'; //talvez tenha que fazer só quando o estado é vazio
+                    tabuleiro[i][colRainhaRetirada].estado = 'm';
+                } else {
+                    //esse i tem que ser igual a 0
+                    for(let j=0; j<tabuleiro[i].length; j++) {
+                        if (tabuleiro[i][j].estado == 'o') {
+                            tabuleiro[i][j].estado = 'm';
+                            colRainhaRetirada = j;
                         }
+                        tabuleiro[i][j].diagD = -1;
+                        tabuleiro[i][j].diagE = -1;
                     }
                 }
-                colunas = JSON.parse(JSON.stringify(auxColunas));
-                //enviar a rainha que foi retirada
-                //desenha(rainhas, i, colunas, "retira");
-                colunas[coluna].estado = 'm';
-                colunas[coluna].diagD = -1;
-                colunas[coluna].diagE = -1;
-                rainhas.pop();
+                rainhas.push({lin: i, col: colRainhaRetirada});
             }
         }
 	}
